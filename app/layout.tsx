3ca0d2c0600d79757f27972ebb7dff788038ed0a
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-05-22 20:38:32 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Summarist - AI-Powered PDF Summarization",
+  description:
+    "Save hours of reading time. Transform lengthy PDFs into clear, accurate Summaries in seconds with our advanced AI technology",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
