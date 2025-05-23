66771082a6becf5835120ef8888fdb98790cebc9
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-05-23 19:45:35 +0530

    hero-section created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/BgGradient";
+import HeroSection from "@/components/home/HeroSection";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
-      <Button variant="outline">Click Me</Button>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+      {/* <DemoSection/> */}
+      {/* <HowItWorkSection/> */}
+      {/* <PricingSection/> */}
+      {/* <CTASection/> */}
     </div>
   );
 }
