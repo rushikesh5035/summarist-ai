936ef014851934c0916dd17e864c797ac33f478d
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-05-24 16:21:50 +0530

    how-it-work and pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import BgGradient from "@/components/common/BgGradient";
 import DemoSection from "@/components/home/DemoSection";
 import HeroSection from "@/components/home/HeroSection";
+import HowItWorkSection from "@/components/home/HowItWorkSection";
+import PricingSection from "@/components/home/PricingSection";
 
 export default function Home() {
   return (
@@ -8,10 +10,10 @@ export default function Home() {
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
+        <DemoSection />
+        <HowItWorkSection />
+        <PricingSection />
       </div>
-      <DemoSection />
-      {/* <HowItWorkSection/> */}
-      {/* <PricingSection/> */}
       {/* <CTASection/> */}
     </div>
   );
