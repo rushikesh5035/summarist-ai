f64e97a372e7694ef049864e8d3e5fb9ee41634e
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2026-03-18 14:58:41 +0530

    feat: create new inngest client and enable inngest realtime middleware

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,7 +1,9 @@
+import { realtimeMiddleware } from "@inngest/realtime/middleware";
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "summarist",
-  eventKey: process.env.INNGEST_EVENT_KEY, // Required for production
+  eventKey: process.env.INNGEST_EVENT_KEY,
+  middleware: [realtimeMiddleware()],
 });
